dc17d64157819414daba42e68740d69254b75c43
Woodpeaker <49099503+Woodpeaker@users.noreply.github.com>
2021-10-09 23:22:05 -0400

    task 1 number 7 done

--- Task2_3.py
+++ Task2_3.py
@@ -1,12 +1,7 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 import pandas as pd
 import matplotlib.pyplot as plt
 
 df = pd.read_csv('drug200.csv')
-# print(df.to_string())
 
 names = ['DrugA', 'DrugB', 'DrugC', 'DrugX', 'DrugY']
 countA = 0
--- task1.py
+++ task1.py
@@ -1,102 +1,97 @@
-# import glob
-# import os
-# #Business
-# file_list = glob.glob(os.path.join(os.getcwd(), "BBC/business", "*.txt"))
-#
-# businessCorpus = []
-#
-# for file_path in file_list:
-#     with open(file_path) as f_input:
-#         businessCorpus.append(f_input.read().encode('latin-1'))
-#
-# #Entertainment
-# file_list = glob.glob(os.path.join(os.getcwd(), "BBC/entertainment", "*.txt"))
-#
-# entertainmentCorpus = []
-#
-# for file_path in file_list:
-#     with open(file_path) as f_input:
-#         entertainmentCorpus.append(f_input.read().encode('latin-1'))
-#
-# #Politics
-# file_list = glob.glob(os.path.join(os.getcwd(), "BBC/politics", "*.txt"))
-#
-# politicsCorpus = []
-#
-# for file_path in file_list:
-#     with open(file_path) as f_input:
-#         politicsCorpus.append(f_input.read().encode('latin-1'))
-#
-# #Sport
-# file_list = glob.glob(os.path.join(os.getcwd(), "BBC/sport", "*.txt"))
-#
-# sportCorpus = []
-#
-# for file_path in file_list:
-#     with open(file_path) as f_input:
-#         sportCorpus.append(f_input.read().encode('latin-1'))
-#
-# #Tech
-# file_list = glob.glob(os.path.join(os.getcwd(), "BBC/tech", "*.txt"))
-#
-# techCorpus = []
-#
-# for file_path in file_list:
-#     with open(file_path) as f_input:
-#         techCorpus.append(f_input.read().encode('latin-1'))
-#
-#
-# #4)
-# from sklearn.feature_extraction.text import CountVectorizer
-# vectorizer = CountVectorizer(encoding='latin-1')
-#
-# X1 = vectorizer.fit_transform(businessCorpus)
-# Dictionary = vectorizer.get_feature_names_out()
-# y1 = vectorizer.fit_transform(Dictionary)
-#
-#
-# # X2 = vectorizer.fit_transform(entertainmentCorpus)
-# # print(X2.toarray())
-# #
-# # X3 = vectorizer.fit_transform(politicsCorpus)
-# # print(X3.toarray())
-# #
-# # X4 = vectorizer.fit_transform(sportCorpus)
-# # print(X4.toarray())
-# #
-# # X5 = vectorizer.fit_transform(techCorpus)
-# # print(X5.toarray())
-#
-# #5)
-# from sklearn.model_selection import train_test_split
-# X_train, X_test = train_test_split(X1, test_size=0.20, random_state=None)
-# print(X_train)
-#
-# #6)
-# from sklearn.naive_bayes import MultinomialNB
-# clf = MultinomialNB()
-# # clf.fit(X_train,)
-# # print(clf.predict(X1))
 from sklearn.datasets import load_files
-
-corpus = load_files('BBC', load_content=True, encoding='latin-1')
-
-X, y = corpus.data, corpus.target
-
-# print(corpus)
-
 from sklearn.feature_extraction.text import CountVectorizer
-vectorizer = CountVectorizer(encoding='latin-1')
-
-X = vectorizer.fit_transform(X).toarray()
-print(vectorizer.get_feature_names_out())
-print(X)
-
 from sklearn.model_selection import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20, random_state=None)
-# print(X_train)
-
 from sklearn.naive_bayes import MultinomialNB
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import classification_report
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import f1_score
+import pandas as pd
+
+# 3)
+corpus = load_files('BBC', load_content=True, encoding='latin-1')
+X_data, y = corpus.data, corpus.target
+
+# 4)
+vectorizer = CountVectorizer(encoding='latin-1')
+X_vectorized = vectorizer.fit_transform(X_data)
+X = X_vectorized.toarray()
+dictionary = vectorizer.get_feature_names_out()
+# print(vectorizer.get_feature_names_out())
+# print(X)
+
+# 5)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20, random_state=None)
+
+# 6)
 clf = MultinomialNB()
 clf.fit(X_train, y_train)
-print(clf.predict(X_test))
+
+# 7)
+print('**************MultinomialNB default values, try 1**************')
+y_pred = clf.predict(X_test)
+
+print('(b)')
+print(confusion_matrix(y_test, y_pred))
+
+print('(c)')
+print(classification_report(y_test, y_pred, target_names=corpus.target_names))
+
+print('(d)')
+print(accuracy_score(y_test, y_pred))
+print(f1_score(y_test, y_pred, average='macro'))
+print(f1_score(y_test, y_pred, average='weighted'))
+
+print('(e)')
+print('Prior probability')
+print('P(Business) = 1/5\n'
+      'P(Entertainment) = 1/5\n'
+      'P(Politics) = 1/5\n'
+      'P(Sport) =  1/5\n'
+      'P(Tech) = 1/5\n')
+
+print('(f)')
+print(dictionary.size)
+
+print('(g)')
+classIdx = 0
+for currClass in corpus.target_names:
+    count = 0
+    for dat in X[y == classIdx]:
+        for num in dat:
+            count = count + num
+    print(count)
+    classIdx = classIdx + 1
+
+print('(h)')
+count = 0
+for dat in X:
+    for num in dat:
+        count = count + num
+print(count)
+
+print('(i)')
+classIdx = 0
+for currClass in corpus.target_names:
+    freq = 0
+    idx = 0
+    for word in dictionary:
+        if sum(X[y == classIdx, idx]) == 0:
+            freq = freq + 1
+        idx = idx + 1
+    print(freq/dictionary.size)
+    classIdx = classIdx + 1
+
+print('(j)')
+classIdx = 0
+for currClass in corpus.target_names:
+    freq = 0
+    idx = 0
+    for word in dictionary:
+        if sum(X[y == classIdx, idx]) == 1:
+            freq = freq + 1
+        idx = idx + 1
+    print(freq/dictionary.size)
+    classIdx = classIdx + 1
+
+print('(k)')
